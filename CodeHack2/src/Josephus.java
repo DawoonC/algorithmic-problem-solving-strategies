f132613171431f2bd49c9899e43d0ca7fe76c23a
yeoshim <yeoshim@gmail.com>
2016-05-12 23:24:07 +0900

    Josephus

---
+++ CodeHack2/src/Josephus.java
@@ -0,0 +1,34 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+//	6 3
+//	123456 -> (1)23456 -> (1)23(4)56 -> (1)(2)3(4)56 -> (1)(2)3(4)5(6)
+public class Josephus {
+    private static Scanner sc;
+    
+	public static void main(String[] args) {
+        sc = new Scanner(System.in);
+        int cases = sc.nextInt();
+        
+        while(cases-- > 0) {
+        	josephus( sc.nextInt(), sc.nextInt() );
+        }
+	}
+
+	private static void josephus(int numMans, int interval) {
+		LinkedList<Integer> survivors = new LinkedList<Integer>();
+		for (int i = 1; i <= numMans; i++) {
+			survivors.add(i);
+		}
+		int killIdx = 0;
+		
+		while( survivors.size() > 2 )	{
+			int idx = killIdx %= survivors.size();
+			survivors.remove( idx );
+			killIdx += (interval-1);
+		}
+		
+		System.out.println( survivors.getFirst() + " " + survivors.getLast() );
+	}
+
+}
