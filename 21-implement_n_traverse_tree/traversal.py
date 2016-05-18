a98a5d97b33de53d11dbefcb6887321c5ce4bb21
Dawoon Choi <cdw_3@naver.com>
2016-05-18 21:50:35 +0900

    added traversal solution

---
+++ 21-implement_n_traverse_tree/traversal.py
@@ -0,0 +1,33 @@
+# Problem ID: TRAVERSAL
+# https://algospot.com/judge/problem/read/TRAVERSAL
+# 31ms
+
+def readline():
+    return raw_input()
+
+
+def calculate_postorder(preorder, inorder, postorder):
+    tree_length = len(preorder)
+    if tree_length == 0:
+        return
+    root = preorder[0]
+    l_partition = inorder.index(root)
+    calculate_postorder(preorder[1:l_partition+1], inorder[:l_partition], postorder)
+    calculate_postorder(preorder[l_partition+1:], inorder[l_partition+1:], postorder)
+    postorder.append(root)
+    return
+
+
+def traversal():
+    total_tests = int(readline())
+    for testcase in range(total_tests):
+        n = int(readline())
+        preorder = readline().split()
+        inorder = readline().split()
+        postorder = []
+        calculate_postorder(preorder, inorder, postorder)
+        print ' '.join(postorder)
+
+
+if __name__ == '__main__':
+    traversal()  # run
