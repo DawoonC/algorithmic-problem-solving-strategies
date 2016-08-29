acc7a8d2c1e7e0c242bc7835ce37740076676cee
yeoshim <yeoshim@gmail.com>
2016-08-30 01:10:19 +0900

    review

--- CodeHack2/src/com/codehack/bst/Insertion.java
+++ CodeHack2/src/com/codehack/bst/Insertion.java
@@ -48,14 +48,17 @@ public class Insertion {
 		return kth( root.right, k-leftSize-1 );
 	}
 
+	//	키값 비교 => 우선순위 비교
 	private static TreapNode delete(TreapNode root, int key) {
 		if( root == null )	return root;
 		
 		if( root.key == key )	{
-			TreapNode ret = merge( root.left, root.right );
-			return ret;
+			//	root 삭제 후 남은 sub-tree들을 병합
+			TreapNode newRoot = merge( root.left, root.right );
+			return newRoot;
 		}
 		
+		//	삭제할 node를 찾으러 recursion
 		if( key < root.key )	{
 			root.setLeft( delete(root.left, key) );
 		}
@@ -66,25 +69,31 @@ public class Insertion {
 		return root;
 	}
 
-	private static TreapNode merge(TreapNode a, TreapNode b) {
-		if( a == null )	return b;
-		if( b == null )	return a;
-		if( a.priority < b.priority )	{
-			b.setLeft( merge(a, b.left) );
-			return b;
+	//	
+	private static TreapNode merge(TreapNode leftTreap, TreapNode rightTreap) {
+		if( leftTreap == null )	return rightTreap;
+		if( rightTreap == null )	return leftTreap;
+		
+		//	rightTreap이 root가 되어야 함 (leftTreap은 어디로? 그래서 merge가 필요)
+		//	leftTreap과 rightTreap의 left가 merge 되어야 함
+		if( leftTreap.priority < rightTreap.priority )	{
+			rightTreap.setLeft( merge(leftTreap, rightTreap.left) );
+			return rightTreap;
 		}
 		
-		a.setRight( merge(a.right, b) );
-		return a;
+		//	leftTreap이 root가 되어야 함
+		leftTreap.setRight( merge(leftTreap.right, rightTreap) );
+		return leftTreap;
 	}
 
+	//	우선순위 비교 => 키값 비교
 	private static TreapNode insert(TreapNode root, TreapNode node) {
 		if( root == null )	return node;	//	base condition
 		
 		if( root.priority < node.priority )	{	//	replace node by new node
 			NodePair splitted = split( root, node.key );
-			node.setLeft( splitted.first );
-			node.setRight( splitted.second );
+			node.setLeft( splitted.smaller );
+			node.setRight( splitted.bigger );
 			return node;
 		}
 		else if( node.key < root.key )	{	//	node < root => insert sub-left w/ recursion 
@@ -99,27 +108,34 @@ public class Insertion {
 
 	private static NodePair split(TreapNode root, int key) {
 		if( root == null )	return new NodePair( null, null );
+		//	node가 없을때까지 recursion
+		//	최종 결과: new 기준, 작은트립과 큰트립으로 나뉨(기존 root는 어딘가에 포함, 더이상 root가 아님)
 		
 		if( root.key < key )	{
+			//	root보다 new가 크면, new보다 큰것을 찾기위해 right를 split
+			//	split 후 큰것은 new에 주고, 작은것은 자신의 오른쪽에(right를 split했으므로)
 			NodePair rs = split( root.right, key );
-			root.setRight( rs.first );
-			return new NodePair( root, rs.second );
+			root.setRight( rs.smaller );
+			return new NodePair( root, rs.bigger );
 		}
-		
+
+		//	root보다 new가 작으면, new보다 작은것을 찾기위해 left를 split
+		//	split 후 작은것은 new에 주고, 큰것은 자신의 왼쪽에(left를 split했으므로)
 		NodePair ls = split( root.left, key );
-		root.setLeft( ls.second );
-		return new NodePair( ls.first, root );
+		root.setLeft( ls.bigger );
+		return new NodePair( ls.smaller, root );
 	}
 
 }
 
+//	first: treap nodes < new.key, second: treap nodes >= new.key
 class NodePair	{
-	public TreapNode first;
-	public TreapNode second;
+	public TreapNode smaller;
+	public TreapNode bigger;
 
 	public NodePair(TreapNode first, TreapNode second) {
-		this.first = first;
-		this.second = second;
+		this.smaller = first;
+		this.bigger = second;
 	}
 	
 }
