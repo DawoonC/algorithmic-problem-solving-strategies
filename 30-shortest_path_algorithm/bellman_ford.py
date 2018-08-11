0a78c390af43564cb3d2bcf0496f8e659df569ec
Dawoon Choi <saffron3@classting.com>
2018-08-11 16:42:00 +0900

    re-implemented Bellman-Ford algorithm

--- 30-shortest_path_algorithm/bellman_ford.py
+++ 30-shortest_path_algorithm/bellman_ford.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 
+
 def bellman_ford(g, start, num_of_v):
     upper_dist = defaultdict(lambda: float('inf'))
     upper_dist[start] = 0
@@ -10,8 +11,8 @@ def bellman_ford(g, start, num_of_v):
         updated = False
         for v in g.keys():
             for w in g[v].keys():
-                if upper_dist[w] > upper_dist[v]+g[v][w]:
-                    upper_dist[w] = upper_dist[v]+g[v][w]
+                if upper_dist[w] > upper_dist[v] + g[v][w]:
+                    upper_dist[w] = upper_dist[v] + g[v][w]
                     updated = True
         # if there is no vertex to update, then break
         if not updated:
@@ -21,3 +22,26 @@ def bellman_ford(g, start, num_of_v):
     if updated:
         return None
     return upper_dist
+
+
+def bellman_ford_new(start, graph):
+    dist = defaultdict(lambda: float('inf'))
+    dist[start] = 0
+    n = len(graph.keys())
+    updated = False
+    for _ in xrange(n):
+        updated = False
+        for curr in graph.keys():
+            for neighbor, w in graph[curr].items():
+                if (dist[curr] + w) < dist[neighbor]:
+                    dist[neighbor] = dist[curr] + w
+                    updated = True
+        if not updated:
+            break
+    # if there is no negative cycle in a graph
+    # nth iteration will not be updated
+    # so if it's updated after nth iteration,
+    # it means there is a negative cycle in a graph.
+    if updated:
+        return 'there is a negative cycle in a graph!'
+    return dist
