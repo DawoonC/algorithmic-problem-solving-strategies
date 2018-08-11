b3c91626dbf066b028a182548a5e162b0957d2bf
Dawoon Choi <saffron3@classting.com>
2018-08-11 17:20:51 +0900

    refactored a solution for the `TIMETRIP` problem

--- 30-shortest_path_algorithm/timetrip.py
+++ 30-shortest_path_algorithm/timetrip.py
@@ -1,57 +1,54 @@
 # Problem ID: TIMETRIP
 # https://algospot.com/judge/problem/read/TIMETRIP
-# 725ms
+# took 192ms
 
 from collections import defaultdict
+
 
 def readline():
     return raw_input()
 
 
-def bellman_ford(g, start, end, num_of_v):
-    upper_dist = defaultdict(lambda: float('inf'))
-    upper_dist[start] = 0
-    v_list = range(num_of_v)
-    path = {start: (start,)}
-    for _ in v_list:
-        for v in g.keys():
-            for w in g[v].keys():
-                if upper_dist[w] > upper_dist[v]+g[v][w]:
-                    upper_dist[w] = upper_dist[v]+g[v][w]
-                    path[w] = path[v]+(w,)
-    for v in g.keys():
-        for w in g[v].keys():
-            if upper_dist[v] + g[v][w] < upper_dist[w]:
-                if end in path and v in path[end] and w in path[end]:
-                    return None
-    return upper_dist
-
-
-def make_edge(a, b, d, graph, inverted_graph):
-    d = int(d)
-    if b not in graph[a] or d < graph[a][b]:
-        graph[a][b] = d
-    if b not in inverted_graph[a] or -d < inverted_graph[a][b]:
-        inverted_graph[a][b] = -d
+def timetrip_bellman_ford(start, graph, n):
+    max_dist = defaultdict(lambda: -float('inf'))
+    min_dist = defaultdict(lambda: float('inf'))
+    max_dist[start] = min_dist[start] = 0
+    max_updated = False
+    min_updated = False
+    for _ in xrange(n):
+        max_updated = min_updated = False
+        for curr in graph.keys():
+            for neighbor in graph[curr].keys():
+                for w in graph[curr][neighbor]:
+                    if (min_dist[curr] + w) < min_dist[neighbor]:
+                        min_dist[neighbor] = min_dist[curr] + w
+                        min_updated = True
+                    if (max_dist[curr] + w) > max_dist[neighbor]:
+                        max_dist[neighbor] = max_dist[curr] + w
+                        max_updated = True
+        if not max_updated and not min_updated:
+            break
+    return (
+        None if min_updated else min_dist,
+        None if max_updated else max_dist,
+    )
 
 
 def timetrip():
     total_tests = int(readline())
-    for testcase in range(total_tests):
-        gx, wh = map(int, readline().split())
-        graph = defaultdict(lambda: defaultdict(int))
-        inverted_graph = defaultdict(lambda: defaultdict(int))
-        for _ in range(wh):
-            a, b, d = readline().split()
-            make_edge(a, b, d, graph, inverted_graph)
-        dist = bellman_ford(graph, '0', '1', gx)
-        shortest = 'INFINITY' if dist == None else dist['1']
-        if shortest == float('inf'):
+    for _ in xrange(total_tests):
+        n_v, n_e = map(int, readline().split())
+        graph = defaultdict(lambda: defaultdict(list))
+        for _ in xrange(n_e):
+            u, v, w = map(int, readline().split())
+            graph[u][v].append(w)
+        min_dist, max_dist = timetrip_bellman_ford(0, graph, n_v)
+        min_result = 'INFINITY' if min_dist is None else min_dist[1]
+        max_result = 'INFINITY' if max_dist is None else max_dist[1]
+        if min_result == float('inf'):
             print 'UNREACHABLE'
-            continue
-        inverted_dist = bellman_ford(inverted_graph, '0', '1', gx)
-        longest = 'INFINITY' if inverted_dist == None else -inverted_dist['1']
-        print shortest, longest
+        else:
+            print min_result, max_result
 
 
 if __name__ == '__main__':
