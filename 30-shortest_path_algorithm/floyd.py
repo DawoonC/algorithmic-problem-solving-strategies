d71a7424c69087c3a54c00afcebad34f3ade4b2f
Dawoon Choi <saffron3@classting.com>
2018-08-11 20:44:01 +0900

    re-implemented Floyd algorithm

--- 30-shortest_path_algorithm/floyd.py
+++ 30-shortest_path_algorithm/floyd.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def init_graph(v_list):
     return [[(0 if i == j else float('inf')) for j in v_list] for i in v_list]
 
@@ -10,3 +13,17 @@ def floyd(graph, v_list):
                 for j in v_list:
                     graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
     return graph
+
+
+def floyd_new(graph):
+    dist = defaultdict(lambda: defaultdict(lambda: float('inf')))
+    for curr in graph.keys():
+        dist[curr][curr] = 0
+        for neighbor in graph[curr].keys():
+            dist[curr][neighbor] = graph[curr][neighbor]
+    nodes = graph.keys()
+    for k in nodes:
+        for u in nodes:
+            for v in nodes:
+                dist[u][v] = min(dist[u][v], dist[u][k] + dist[k][v])
+    return dist
